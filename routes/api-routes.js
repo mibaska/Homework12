a251c023e8ea02d2876aeb04782cda7484d0bba3
Michael Baska <mikebaska1@gmail.com>
2020-04-12 20:33:41 -0500

    nearly got it

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,8 +12,29 @@ router.get("/api/workouts/", (req, res) => {
     });
 });
 
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.updateOne({ _id: req.params.id })
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+});
+
 router.post("/api/workouts/", ({ body }, res) => {
   Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
